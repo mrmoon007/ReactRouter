60f64695e4af290c572364d6fc51d34544b48cf7
mostafijur007 <mostafijur.techvill@gmail.com>
2023-08-21 22:32:55 +0600

    Added route navigate functionality.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function About() {
+    const navigate = useNavigate();
   return (
     <div className='container'>
       <h3>About</h3>
       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae nisi natus fugit nemo voluptatum repellat, placeat ullam saepe hic animi adipisci repudiandae nesciunt repellendus? Magnam tempora eaque minima rerum est?</p>
+
+      <button onClick={()=> navigate('/')}>back to home</button>
     </div>
   )
 }
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function Contact() {
+    const navigate = useNavigate();
   return (
     <div className='container'>
       <h3>Contact</h3>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam ipsum accusantium repellendus nesciunt atque doloremque inventore pariatur nam eum at debitis dolore voluptatum eius recusandae non, architecto vitae minus repellat?</p>
+
+      <button onClick={()=> navigate('/')}>back to home</button>
     </div>
   )
 }
